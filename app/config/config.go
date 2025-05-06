29c03080cfb714542d457f47169ef89de3b23b1a
onion0904 <takerudaze0904@gmain.com>
2025-05-07 06:52:16 +0900

    deploy test

--- app/config/config.go
+++ app/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"os"
 	"sync"
+	"log"
 )
 
 type Config struct {
@@ -39,16 +40,26 @@ var (
 func GetConfig() *Config {
 	// goroutine実行中でも一度だけ実行される
 	once.Do(func() {
+		log.Println("Initializing config...")
 		config.DB.DB_URL = getEnv("DATABASE_URL", "")
+		log.Println("DB_URL:", config.DB.DB_URL)
 
 		// Server
-		config.Server.Port = getEnv("SERVER_PORT", "8080")
+		config.Server.Port = getEnv("SERVER_PORT", "")
+		log.Println("SERVER_PORT:", config.Server.Port)
 
 		// Mailgun
 		config.Mailgun.Domain = getEnv("MAILGUN_DOMAIN", "")
+		log.Println("MAILGUN_DOMAIN:", config.Mailgun.Domain)
+
 		config.Mailgun.Private_Key = getEnv("MAILGUN_PRIVATE_API_KEY", "")
+		log.Println("MAILGUN_PRIVATE_API_KEY:", config.Mailgun.Private_Key)
+
 		config.Mailgun.Sender_email = getEnv("SENDER_EMAIL", "")
+		log.Println("SENDER_EMAIL:", config.Mailgun.Sender_email)
+
 		config.Mailgun.Recipient_email = getEnv("RECIPIENT_EMAIL", "")
+		log.Println("RECIPIENT_EMAIL:", config.Mailgun.Recipient_email)
 
 		// JWT
 		config.JWT.Secret = getEnv("JWT_SECRET", "")
